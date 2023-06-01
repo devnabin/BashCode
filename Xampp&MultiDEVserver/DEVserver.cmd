e947118d64f4ad4a21639179a35eda2268de4134
Nabin Bhandari <46418794+devnabin@users.noreply.github.com>
2023-06-01 09:29:29 +0545

    CMD TRICK FOR MULTI DEVSERVER

---
+++ Xampp&MultiDEVserver/DEVserver.cmd
@@ -0,0 +1,7 @@
+start cmd /k "cd C:\xampp && apache_start.bat"
+start cmd /k "cd C:\xampp && mysql_start.bat"
+start cmd /k "cd computoroperator && npm run dev"
+start cmd /k "cd nabinprotfoliobackend && npm run dev"
+start cmd /k "cd BCH-FrontEnd && npm run dev"
+
+
